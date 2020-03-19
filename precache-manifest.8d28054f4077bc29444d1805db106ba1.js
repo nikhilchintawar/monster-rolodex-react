ff76301e2acf8056b31372f3d7e330a1f0778d08
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-03-19 05:59:31 +0530

    Updates

---
+++ precache-manifest.8d28054f4077bc29444d1805db106ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4a4e4eaf1712a13c0c1b6eb6532dd4",
+    "url": "/monster-rolodex-react/index.html"
+  },
+  {
+    "revision": "16e3e0cb433c6504c89a",
+    "url": "/monster-rolodex-react/static/css/main.33118f8e.chunk.css"
+  },
+  {
+    "revision": "c30e57748e3eb8d29aa4",
+    "url": "/monster-rolodex-react/static/js/2.a626d1d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex-react/static/js/2.a626d1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e3e0cb433c6504c89a",
+    "url": "/monster-rolodex-react/static/js/main.35798522.chunk.js"
+  },
+  {
+    "revision": "3d7a8d11cce2997065b1",
+    "url": "/monster-rolodex-react/static/js/runtime-main.acc7e037.js"
+  }
+]);
